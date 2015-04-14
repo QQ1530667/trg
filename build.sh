3d542112bda56142a95e6e8be11c2b5286f9cd28
Michael Barbour <barbour.michael.0@gmail.com>
2015-04-13 23:30:24 -0400

    Improve build script.

--- build.sh
+++ build.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
 
 fail() {
-	echo ">> Failed"
+	echo "## failed"
 	exit $1
 }
 
 [ -z "$CC" ] && CC="gcc"
 
-CFLAGS="-Os -Wall $CFLAGS"
-LDFLAGS="-lm $LDFLAGS"
-EXEC_NAME="wkb"
-
-WEBKIT_PKG=webkit2gtk-3.0
 if pkg-config --exists webkit2gtk-4.0; then
 	WEBKIT_PKG=webkit2gtk-4.0
+elif pkg-config --exists webkit2gtk-3.0; then
+	WEBKIT_PKG=webkit2gtk-3.0
+else
+	echo "## error: wkb requires webkit2gtk"
+	fail
 fi
 
 [ "$WITH_SHARED" = "y" ] && CFLAGS="$CFLAGS -DWITH_SHARED"
 
-CFLAGS="$CFLAGS $(pkg-config --cflags gtk+-3.0 $WEBKIT_PKG)"
-LDFLAGS="$LDFLAGS $(pkg-config --libs gtk+-3.0 $WEBKIT_PKG)"
+CFLAGS="-Os -Wall $(pkg-config --cflags gtk+-3.0 $WEBKIT_PKG) $CFLAGS"
+LDFLAGS="$(pkg-config --libs gtk+-3.0 $WEBKIT_PKG) -lm $LDFLAGS"
 
 echo "CFLAGS: $CFLAGS"
 echo "LDLAGS: $LDFLAGS"
 
-$CC -o $EXEC_NAME wkb.c $CFLAGS $LDFLAGS || fail $?
-[ "$STRIP" = "n" ] || strip -s $EXEC_NAME || echo "strip failed..."
+$CC -o wkb $CFLAGS $LDFLAGS wkb.c || fail $?
 
 echo ">> Build successful"
