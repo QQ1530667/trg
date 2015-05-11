19ead988bd054633f584a5bb289b2c3f2471b816
Michael Barbour <barbour.michael.0@gmail.com>
2015-05-10 22:25:47 -0400

    plugins/download: Use {term} (instead of just xterm) if set.

--- extras/plugins/download/download_open.sh
+++ extras/plugins/download/download_open.sh
@@ -2,14 +2,15 @@
 
 SCRIPT_NAME="$(basename "$0")"
 CMD_FIFO="$1"
-FILE="$2"
+TERM="${2:-xterm}"
+FILE="$3"
 if ! [ -n "$CMD_FIFO" -a -n "$FILE" -a -e "$FILE" ]; then
 	[ -n "$CMD_FIFO" -a -p "$CMD_FIFO" ] && echo "echo $SCRIPT_NAME: error: file does not exist: \"$FILE\"" > "$CMD_FIFO"
 	exit 1
 fi
 
 rm_prompt() {
-	xterm -e "$(dirname "$0")/rm_prompt.sh" "$1"
+	$TERM -e "$(dirname "$0")/rm_prompt.sh" "$1"
 }
 
 case "$(file -b --mime-type "$FILE")" in
