8e9488c66e58fae0ffb468dec7e8ad63d9fe0862
Michael Barbour <barbour.michael.0@gmail.com>
2015-07-10 00:03:21 -0400

    img_hover: Add imgflip.com support.

---
+++ extras/plugins/img_hover/plugins/imgflip_a.js
@@ -0,0 +1,19 @@
+var img_hover_plugins = img_hover_plugins || [];
+img_hover_plugins.push({
+	name:'imgflip_a',
+	prepare_links:function(links) {
+		for (var i = 0; i < links.length; ++i) {
+			var matches = links[i].href.match(/^[A-Za-z0-9+.-]+:\/\/(?:www\.)?imgflip\.com\/i\/([a-zA-Z0-9]+)/);
+			if (matches && matches[1]) {
+				var image_url = 'http://i.imgflip.com/' + matches[1] + '.jpg';
+				links[i].addEventListener('mouseover', {
+						element:links[i],
+						href:image_url,
+						handleEvent:function(event) {
+							img_hover.display_image(this.element, this.href);
+						}
+					}, false);
+			}
+		}
+	}
+});
