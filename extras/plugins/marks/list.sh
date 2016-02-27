4e1588dfc2e541e02f212c304269a5e52cc375d0
Michael Barbour <barbour.michael.0@gmail.com>
2016-02-27 02:16:23 -0500

    plugins/marks: Use tabs in list.sh output.

--- extras/plugins/marks/list.sh
+++ extras/plugins/marks/list.sh
@@ -4,8 +4,8 @@ SCRIPT_NAME="$(basename "$0")"
 cd "$(dirname "$0")"
 . ./common.sh
 
-[ -n "$1" ] || fail "echo $SCRIPT_NAME: usage: $SCRIPT_NAME <file> <command> [regex-]"
+[ -n "$1" ] || fail "echo $SCRIPT_NAME: usage: $SCRIPT_NAME <marks_file> [regex-]"
 FILE="$1"
 shift 1
 [ -f "$FILE" ] || fail "echo $SCRIPT_NAME: error: marks file \\\"$FILE\\\" does not exist"
-grep -e "$*" < "$FILE" | sed -e 's/^/echo /' | escape
+grep -e "$*" < "$FILE" | sed -e 's/ /\t/' -e 's/^/echo /' | escape | sed -e 's/^echo /echo "/' -e 's/$/"/'
